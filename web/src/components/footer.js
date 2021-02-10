5ff6687e867d5224dfc7587af2db22c3b157bed7
Gui Hebling <guilherme.durso@outlook.com>
2021-02-10 20:23:12 +0100

    Create footer structure

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterWrapper = styled.footer``;
+
+const FooterColumn = styled.div``;
+
+const FooterTitle = styled.h4``;
+
+const FooterItem = styled.p``;
+
+const FooterSocialMedia = styled.div``;
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <FooterWrapper>
+      <FooterColumn>
+        <FooterTitle>Footer</FooterTitle>
+        <FooterItem>email</FooterItem>
+        <FooterItem>phone</FooterItem>
+      </FooterColumn>
+
+      <FooterColumn>
+        <FooterTitle>Footer</FooterTitle>
+        <FooterItem>email</FooterItem>
+        <FooterItem>phone</FooterItem>
+      </FooterColumn>
+
+      <FooterColumn>
+        <FooterSocialMedia>f i li</FooterSocialMedia>
+      </FooterColumn>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
