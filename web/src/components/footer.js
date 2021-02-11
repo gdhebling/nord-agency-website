9a4b2b51a34122fdef90007b7184347e71a1e8ce
Gui Hebling <guilherme.durso@outlook.com>
2021-02-11 18:44:54 +0100

    Finish basis of the Footer component

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -1,33 +1,80 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { StyledIconBase } from '@styled-icons/styled-icon';
+import { FacebookF, Instagram, LinkedinIn } from '@styled-icons/fa-brands';
 
-const FooterWrapper = styled.footer``;
+import { FaFacebookF, FaLinkedinIn, FaInstagram } from 'react-icons/fa';
 
-const FooterColumn = styled.div``;
+const FooterWrapper = styled.footer`
+  height: 40vh;
+  align-items: center;
+  justify-content: space-evenly;
+  padding: 3rem;
+  display: flex;
+  background: #f6f7fa;
+`;
 
-const FooterTitle = styled.h4``;
+const FooterColumn = styled.div`
+  text-align: left;
+`;
 
-const FooterItem = styled.p``;
+const FooterTitle = styled.h4`
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin: 1rem 0;
+`;
 
-const FooterSocialMedia = styled.div``;
+const FooterItem = styled.p`
+  font-size: 1rem;
+  margin: 0.5rem 0;
+`;
+
+const StyledSocialMediaIcons = css`
+  cursor: pointer;
+  font-size: 2rem;
+  margin: 0 0.5rem;
+`;
+
+const StyledFacebookIcon = styled(FaFacebookF)`
+  ${StyledSocialMediaIcons}
+`;
+const StyledLinkedInIcon = styled(FaLinkedinIn)`
+  ${StyledSocialMediaIcons}
+`;
+const StyledInstagramIcon = styled(FaInstagram)`
+  ${StyledSocialMediaIcons}
+`;
+
+/* const FooterSocialMedia = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+
+  ${StyledIconBase} {
+    margin: 0 0.2rem;
+  }
+`; */
 
 const Footer = () => {
   return (
     <FooterWrapper>
       <FooterColumn>
-        <FooterTitle>Footer</FooterTitle>
-        <FooterItem>email</FooterItem>
-        <FooterItem>phone</FooterItem>
+        <FooterTitle>Contact Us</FooterTitle>
+        <FooterItem>hei@nord.no</FooterItem>
+        <FooterItem>+47 12 34 56 78</FooterItem>
       </FooterColumn>
 
       <FooterColumn>
-        <FooterTitle>Footer</FooterTitle>
-        <FooterItem>email</FooterItem>
-        <FooterItem>phone</FooterItem>
+        <FooterTitle>Visit Us</FooterTitle>
+        <FooterItem>Tollbugata 8</FooterItem>
+        <FooterItem>0152 Oslo, Norway</FooterItem>
       </FooterColumn>
 
       <FooterColumn>
-        <FooterSocialMedia>f i li</FooterSocialMedia>
+        <StyledFacebookIcon />
+        <StyledLinkedInIcon />
+        <StyledInstagramIcon />
       </FooterColumn>
     </FooterWrapper>
   );
