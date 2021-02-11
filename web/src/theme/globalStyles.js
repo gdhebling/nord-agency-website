855a2d07110be5c6d6bf0d5a6880ab9da8cab095
Gui Hebling <guilherme.durso@outlook.com>
2021-02-11 19:57:26 +0100

    Add html reset

--- web/src/theme/globalStyles.js
+++ web/src/theme/globalStyles.js
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
 body {
     font-family: -apple-system, Roboto, sans-serif, serif;
     overflow-x: hidden;
